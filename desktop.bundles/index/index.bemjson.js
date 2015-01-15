a233aeb54a48acc94a7950e42e63bbd04552f013
Alexey <guryanov@kg.ru>
2015-01-15 22:00:05 +0000

    kg-app block introduced
    Replaces itself by page block with predefined attributes. Then mixs own styles on page block.

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,15 +1,6 @@
 ({
-    block: 'page',
+    block: 'kg-app',
     title: 'Demo KG base web app',
-    styles: [
-        { elem: 'css', url: 'index.css' },
-        '<!--[if IE]>',
-            { elem: 'css', url: 'index.ie.css' },
-        '<![endif]-->'
-    ],
-    scripts: [
-        { elem: 'js', url: 'index.js' }
-    ],
     content: [
         'Hello, KG!'
     ]
