6e743697fc8d55a4fe02e7e5dd74816a534d6f23
Alexey <guryanov@kg.ru>
2015-01-17 14:45:36 +0000

    Search field position now independent of sibling elements

--- common.blocks/app-header/app-header.bh.js
+++ common.blocks/app-header/app-header.bh.js
@@ -1,5 +1,26 @@
 module.exports = function(bh) {
-  bh.match('app-header', function(bh) {
-    
+  bh.match('app-header', function(ctx, json) {
+    var content = [
+      null,
+      {
+        elem: 'spacer',
+        content: []
+      },
+      null
+    ];
+    [].concat(ctx.json().content).forEach(function(elem){
+      if(!ctx.isSimple(elem) && ('elem' in elem)) {
+        if(elem.elem == 'logo') {
+          content[0] = elem;
+        } else if (elem.elem == 'actions') {
+          content[2] = elem;
+        } else {
+          content[1].content.push(elem);
+        }
+      } else {
+        content[1].content.push(elem);
+      }
+    });
+    ctx.content(content, true);
   });
 };
